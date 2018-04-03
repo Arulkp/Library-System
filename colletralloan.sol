acab2e02cebc0499edceab71e8d39bead110c8ed
Arulkp <31948738+Arulkp@users.noreply.github.com>
2018-04-03 18:24:50 +0530

    Update colletralloan.sol

--- colletralloan.sol
+++ colletralloan.sol
@@ -4,7 +4,7 @@ import "./Bank.sol";
 import "./EIP20.sol";
 contract Loan_Details is Bank,NewSPToken 
 {
-
+    uint public price=0.01 ether;
     //uint256 public ln_req_count=0;
     struct loan_get
     {
@@ -17,8 +17,8 @@ contract Loan_Details is Bank,NewSPToken
         uint bal_ln;
         uint installment;
         uint tcount;
+        address token_address;
     }
-   
     mapping (address=>mapping(uint256=>loan_get))public ln_get;
     mapping(address=>uint256)public ln_get_count;
     struct loan_pro
@@ -39,23 +39,24 @@ contract Loan_Details is Bank,NewSPToken
         //require(bank_d1[bank_address].time!=0);
         //require(bank_address!=msg.sender);
        //uint256 amt = (eth * tokens);
-       
-        require(msg.value==tokens&& tokens <=balanceOf[msg.sender]);
-        require (bank_d1[bank_address].bal > msg.value );
-                                                             
+        uint amount=tokens*price;   
+        require(amount==tokens&& tokens <=balanceOf[msg.sender]);
+        require (bank_d1[bank_address].bal > amount );
+                                                       
         NewSPToken(_token).transferFrom(msg.sender,bank_address,tokens);
-        bank_d1[msg.sender].bal += msg.value;  //   msg.sender.transfer(msg.value);
-        bank_d1[bank_address].bal -= msg.value;
+        bank_d1[msg.sender].bal += amount;  //  msg.sender.transfer(msg.value);
+        bank_d1[bank_address].bal -= amount;
         ln_get[msg.sender][ln_get_count[msg.sender]].bank_address = bank_address;
-        ln_get[msg.sender][ln_get_count[msg.sender]].amount = msg.value;
+        ln_get[msg.sender][ln_get_count[msg.sender]].amount = amount;
         ln_get[msg.sender][ln_get_count[msg.sender]].months=year*12;
         ln_get[msg.sender][ln_get_count[msg.sender]].time=now;
         ln_get[msg.sender][ln_get_count[msg.sender]].tcount=tokens;
         ln_get[msg.sender][ln_get_count[msg.sender]].last_setl_time=now;
-        ln_get[msg.sender][ln_get_count[msg.sender]].installment=(msg.value)/(year*12);
-        ln_get[msg.sender][ln_get_count[msg.sender]].bal_ln = msg.value;
+        ln_get[msg.sender][ln_get_count[msg.sender]].installment=(amount)/(year*12);
+        ln_get[msg.sender][ln_get_count[msg.sender]].bal_ln = amount;
+        ln_get[msg.sender][ln_get_count[msg.sender]].token_address=_token;
         ln_pro[bank_address][ln_pro_count[bank_address]].bank_address = msg.sender;
-        ln_pro[bank_address][ln_pro_count[bank_address]].amount = msg.value;
+        ln_pro[bank_address][ln_pro_count[bank_address]].amount = amount;
         ln_pro[bank_address][ln_pro_count[bank_address]].months=year*12;
         ln_pro[bank_address][ln_pro_count[bank_address]].tokens=tokens;
         ln_pro[bank_address][ln_pro_count[bank_address]].time=now;
@@ -76,7 +77,10 @@ contract Loan_Details is Bank,NewSPToken
         uint temp_ins=ln_get[msg.sender][ln_id].installment;
         uint temp_last=ln_get[msg.sender][ln_id].last_setl_time + 20 seconds;//30 days;
         address temp_bank_address=ln_get[msg.sender][ln_id].bank_address;
-        uint temp_tcount=ln_get[msg.sender][ln_id].tcount;
+        //uint temp_tcount=ln_get[msg.sender][ln_id].tcount;
+        address _token=ln_get[msg.sender][ln_id].token_address;
+        uint tokens=ln_pro[temp_bank_address][ln_pro_count[temp_bank_address]].tokens;
+        
         if(temp_count<temp_month-1){
         require((temp_last)<=now);
         
@@ -91,6 +95,8 @@ contract Loan_Details is Bank,NewSPToken
         ln_get[msg.sender][ln_id].last_setl_time = temp_last;//30 days;
         ln_get[msg.sender][ln_id].bal_ln-=temp_ins;
         ln_get[msg.sender][ln_id].count++;
+        
+        NewSPToken(_token).transferFrom(temp_bank_address,msg.sender,tokens);
         
         //require( ln_get[msg.sender][ln_id].count==12);
         // bank_d1[msg.sender].tokencount-= bank_d1[msg.sender].tokencount;
